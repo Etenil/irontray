3ae248408bc9a4f1a20976d6e767cf38781c8a21
Guillaume Pasquet <dev@etenil.net>
2016-03-15 14:33:30 +0000

    Updated code for irust 1.7.

--- src/http/traits.rs
+++ src/http/traits.rs
@@ -1,10 +1,10 @@
-pub trait FromU16 {
+pub trait FromU16 : Sized {
     type Err;
     
     fn from_u16(num: u16) -> Result<Self, Self::Err>;
 }
 
-pub trait FromString {
+pub trait FromString : Sized {
     type Err;
     
     fn from_string(s: String) -> Result<Self, Self::Err>;
